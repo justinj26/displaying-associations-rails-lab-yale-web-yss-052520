fc94cbd2400bd9b3d7af1498e8ee8522c0042698
justin_james <jjames11799@gmail.com>
2020-06-24 21:38:06 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+drake = Artist.create(name: 'drake')
+
+
+hotline = Song.create(title: 'hotline', artist_id: drake.id)
+toosie_slide = Song.create(title: 'toosie slide', artist_id: drake.id)
